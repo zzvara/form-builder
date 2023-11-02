e943f6c42ef1cac908ebfa834706239dac2455db
S4x4p <116021839+S4x4p@users.noreply.github.com>
2023-11-02 21:32:49 +0100

    task-6 add date picker

--- form-builder/src/app/app.module.ts
+++ form-builder/src/app/app.module.ts
@@ -16,11 +16,12 @@ import { FormCreatorModule } from './pages/form-creator/form-creator.module';
 import { InputComponent } from './shared/components/input/input.component';
 import { TextareaComponent } from './shared/components/textarea/textarea.component';
 import { SelectComponent } from './shared/components/select/select.component';
+import { DatePickerComponent } from './shared/components/date-picker/date-picker.component';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [AppComponent, InputComponent, TextareaComponent, SelectComponent],
+  declarations: [AppComponent, InputComponent, TextareaComponent, SelectComponent, DatePickerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
