3d49b8a81961ae769581c7c3aef096e09c27c1e8
he-david <hegyidavid2001@gmail.com>
2023-11-03 13:21:15 +0100

    build fiz

--- form-builder/src/app/app.module.ts
+++ form-builder/src/app/app.module.ts
@@ -7,21 +7,18 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardModule } from './pages/dashboard/dashboard.module';
 import { EditModule } from './pages/edit/edit.module';
 import { FormCreatorModule } from './pages/form-creator/form-creator.module';
-import { InputComponent } from './shared/components/input/input.component';
-import { TextareaComponent } from './shared/components/textarea/textarea.component';
-import { SelectComponent } from './shared/components/select/select.component';
-import { DatePickerComponent } from './shared/components/date-picker/date-picker.component';
+import { SharedModule } from './shared/shared.module';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [AppComponent, InputComponent, TextareaComponent, SelectComponent, DatePickerComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -31,6 +28,7 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     DashboardModule,
     EditModule,
+    SharedModule,
     FormCreatorModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
--- form-builder/src/app/shared/components/input/input.component.ts
+++ form-builder/src/app/shared/components/input/input.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-text-input',
-    templateUrl: './text-input.component.html',
-    styleUrls: ['./text-input.component.css']
+  selector: 'app-text-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent {
-    @Input() questionValue!: string;
-    @Input() answer!: Component;
-    @Input() type!: string;
-    @Input() inputPlaceholder!: string;
+  @Input() questionValue!: string;
+  @Input() answer!: Component;
+  @Input() type!: string;
+  @Input() inputPlaceholder!: string;
 }
