b7a0a766d517fd60831603b44d063d5e9b8fa0d1
Dóra Dorogi <un7njr@INF.ELTE.HU>
2023-11-16 18:18:57 +0100

    style

--- form-builder/src/app/pages/form-creator/form-creator.module.ts
+++ form-builder/src/app/pages/form-creator/form-creator.module.ts
@@ -5,13 +5,15 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FormCreatorComponent],
-  imports: [NzFormModule, NzInputModule, NzLayoutModule, NzSwitchModule, NzDatePickerModule, NzCheckboxModule, NzStepsModule, FormsModule, ReactiveFormsModule],
+  imports: [NzFormModule, NzInputModule, NzLayoutModule, NzSwitchModule, NzDatePickerModule, NzTimePickerModule, NzCheckboxModule, NzStepsModule, NzButtonModule, FormsModule, ReactiveFormsModule],
   providers: [],
   exports: [],
 })
