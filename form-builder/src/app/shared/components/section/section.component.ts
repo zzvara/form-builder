1a705e06c825172a11fd18f48155553a45e689a1
vago-blanka <vago.blanka@leafsoft.hu>
2024-01-14 23:37:57 +0100

    Task-14: Create sectionComponent

--- form-builder/src/app/shared/components/section/section.component.ts
+++ form-builder/src/app/shared/components/section/section.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
-  styleUrls: ['./section.component.css']
+  styleUrls: ['./section.component.css'],
 })
 export class SectionComponent {
-  sectiondId:string=`section${Math.random()}`;
+  randomIntFromInterval(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  @Input() sectiondId: string = `section${this.randomIntFromInterval(1, 100)}`;
+  @Input() sectionList: string[] = ['buildedForm'];
+
+  ngOnInit() {
+    this.sectionList.push(this.sectiondId);
+    console.log({ lis: this.sectionList, id: this.sectiondId });
+    JSON.stringify(this.sectiondId);
+  }
 }
