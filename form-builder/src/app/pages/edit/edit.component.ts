f7c33f41a95aeada0d87c34da5f7d0c881591486
vago-blanka <vago.blanka@leafsoft.hu>
2024-01-15 08:25:27 +0100

    Task-20: Undo or redo changed Username value

--- form-builder/src/app/pages/edit/edit.component.ts
+++ form-builder/src/app/pages/edit/edit.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UndoRedoService } from 'src/app/services/undo-redo.service';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./edit.component.css'],
 })
 export class EditComponent {
-  constructor(private readonly router: Router) {}
+  constructor(private readonly router: Router, private undeoRedoService: UndoRedoService) {}
 
   onNavigateBack() {
     this.router.navigate(['/dashboard']);
+  }
+
+  onValueChange(event: any) {
+    this.undeoRedoService.changeEvent(event);
   }
 }
