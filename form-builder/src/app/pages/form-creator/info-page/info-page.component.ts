a733fa30f7c304c521d4062fb80912b1684ee461
hxffner <hofi.marcell@gmail.com>
2024-05-03 11:00:46 +0200

    Reworked info-page/info-page.component.ts.

--- form-builder/src/app/pages/form-creator/info-page/info-page.component.ts
+++ form-builder/src/app/pages/form-creator/info-page/info-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Project, ProjectType } from '../../../items/project.interface';
-import { Question } from '../../../items/question/questions.interface';
 import { ProjectService } from '../../../services/project.service';
 import { FormGroup, FormControl } from '@angular/forms';
 import { NzModalService } from 'ng-zorro-antd/modal';
@@ -34,6 +33,15 @@ export class InfoPageComponent implements OnInit {
   formId = 0;
   saveFailed = false;
 
+  form = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    type: new FormControl(false),
+    deadline: new FormControl(''),
+    haslimit: new FormControl(false),
+    limit: new FormControl(0),
+  });
+
   constructor(
     private readonly modal: NzModalService,
     private readonly route: ActivatedRoute,
@@ -46,8 +54,7 @@ export class InfoPageComponent implements OnInit {
       if (params['id']) {
         this.formExists = true;
         this.formId = Number(params['id']);
-        const projects = JSON.parse(localStorage.getItem('project') || '[]');
-        this.project = projects.find((p: Project) => p.id === this.formId) || null;
+        this.project = this.projectService.searchData(this.formId)[0] || null;
         if (this.project) {
           this.initializeForm();
         }
@@ -61,24 +68,15 @@ export class InfoPageComponent implements OnInit {
   }
 
   initializeForm(): void {
-    this.form = new FormGroup({
-      title: new FormControl(this.project?.title || ''),
-      description: new FormControl(this.project?.description || ''),
-      type: new FormControl(this.project?.type === 'test'),
-      deadline: new FormControl(this.project?.deadline || ''),
-      haslimit: new FormControl(this.project?.time_chechkbox || false),
-      limit: new FormControl(this.project?.time_limit || 0),
+    this.form.patchValue({
+      title: this.project.title || '',
+      description: this.project.description || '',
+      type: this.project.type === ProjectType.TEST,
+      deadline: this.project.deadline || '',
+      haslimit: this.project.time_chechkbox || false,
+      limit: this.project.time_limit || 0,
     });
   }
-
-  form = new FormGroup({
-    title: new FormControl(''),
-    description: new FormControl(''),
-    type: new FormControl(false),
-    deadline: new FormControl(''),
-    haslimit: new FormControl(false),
-    limit: new FormControl(0),
-  });
 
   setDeadline() {
     this.hasdeadline = !this.hasdeadline;
@@ -90,33 +88,23 @@ export class InfoPageComponent implements OnInit {
   }
 
   updateForm() {
-    if (this.project) {
-      this.project.title = this.form.controls['title'].value!;
-      this.project.description = this.form.controls['description'].value!;
-      this.project.type = this.form.controls['type'].value ? ProjectType.TEST : ProjectType.QUESTIONNAIRE;
-      this.project.deadline = this.form.controls['deadline'].value!;
-      this.project.time_chechkbox = this.form.controls['haslimit'].value!;
-      if (this.project.time_chechkbox) {
-        this.project.time_limit = this.form.controls['limit'].value!;
-      }
+    this.project.title = this.form.controls['title'].value!;
+    this.project.description = this.form.controls['description'].value!;
+    this.project.type = this.form.controls['type'].value ? ProjectType.TEST : ProjectType.QUESTIONNAIRE;
+    this.project.deadline = this.form.controls['deadline'].value!;
+    this.project.time_chechkbox = this.form.controls['haslimit'].value!;
+    if (this.project.time_chechkbox) {
+      this.project.time_limit = this.form.controls['limit'].value!;
     }
   }
 
   submitForm() {
     this.updateForm();
-    let projects: Project[] = JSON.parse(localStorage.getItem('project') || '[]');
-
     if (this.formExists && this.formId !== 0) {
-      const index = projects.findIndex((p) => p.id === this.formId);
-      if (index !== -1) {
-        projects[index] = this.project;
-      }
+      this.projectService.update(this.formId, this.project);
     } else {
-      this.project.id = projects.length > 0 ? projects[projects.length - 1].id + 1 : 1;
-      projects.push(this.project);
+      this.projectService.add(this.project);
     }
-
-    localStorage.setItem('project', JSON.stringify(projects));
 
     this.page! += 1;
     this.onsetPage(this.page!);
