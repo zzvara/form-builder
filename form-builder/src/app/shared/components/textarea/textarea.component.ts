afa09bc8b103e600e455a84b7313507e64db6b41
vago-blanka <vago.blanka@leafsoft.hu>
2024-05-09 23:44:32 +0200

    Use layout component in textarea component

--- form-builder/src/app/shared/components/textarea/textarea.component.ts
+++ form-builder/src/app/shared/components/textarea/textarea.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
-    selector: 'app-textarea',
-    templateUrl: './textarea.component.html',
-    styleUrls: ['./textarea.component.css']
+  selector: 'app-textarea',
+  templateUrl: './textarea.component.html',
+  styleUrls: ['./textarea.component.css'],
 })
 export class TextareaComponent {
-    @Input() questionValue!: string;
-    @Input() answerValue!: Component;
-    @Input() textareaPlaceholder!: string;
-    @Input() sectiondId!: string;
+  //questionValue: string = 'Date input';
+  descriptionValue: string = 'The input can be used for...';
+  //answerValue: any = 'Date answer';
+  inputPlaceholder: string = 'Date input value';
+  inputTemplate!: TemplateRef<any>;
+  type: string = 'text';
+  /* @Input() */ questionValue: string = 'Textarea input';
+  @Input() answerValue!: Component;
+  @Input() textareaPlaceholder!: string;
+  @Input() sectiondId!: string;
 }
