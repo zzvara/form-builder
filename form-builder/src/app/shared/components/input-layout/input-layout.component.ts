4678d884e37168b945e55eba28c06aaef07b601d
vago-blanka <vago.blanka@leafsoft.hu>
2024-06-13 23:39:19 +0200

    Add description to input-layout component

--- form-builder/src/app/shared/components/input-layout/input-layout.component.ts
+++ form-builder/src/app/shared/components/input-layout/input-layout.component.ts
@@ -8,7 +8,6 @@ import { Component, EventEmitter, Input, Output, TemplateRef } from '@angular/co
 export class InputLayoutComponent {
   @Input() questionValue!: string;
   @Input() descriptionValue!: string;
-  @Input() answerValue!: any;
   @Input() type: string = 'text';
   @Input() inputPlaceholder: string = '';
   @Input() sectiondId!: string;
@@ -16,6 +15,16 @@ export class InputLayoutComponent {
   isEditingQuestion: boolean = false;
   isEditingDescription: boolean = false;
   isSwitchingAdornment: boolean = false;
+
+  // Input-Layout component gives all the draggable components a layout
+  // There's an option to click on the question and descriptionValue to
+  // change it according to the users' need.
+  // WIP: there are also adornments that would indicate if the current
+  // input element is required or not. For this I created the boolean
+  // variable, but in the current version it does NOT work as it should.
+  // When user clicks on the questionValue the adornment disappears/ or
+  // when user clicks on the adornment it also handles it like the user
+  // wants to change the question
 
   @Output() questionValueChanged = new EventEmitter<string>();
   @Output() descriptionValueChanged = new EventEmitter<string>();
