ce26f9420120ca6502d23a76b693a7a81fcaf16d
hxffner <hofi.marcell@gmail.com>
2024-11-19 08:55:13 +0100

    Added section input statistics

--- form-builder/src/app/pages/form-creator/results-page/results-page.component.ts
+++ form-builder/src/app/pages/form-creator/results-page/results-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Project, ProjectVersion } from 'src/app/interfaces/project';
 import { JsonService } from 'src/app/services/json.service';
 import { ProjectService } from 'src/app/services/project.service';
+import { StatisticsService } from './services/statistics.service';
 
 @Component({
   selector: 'app-results-page',
@@ -15,12 +16,21 @@ export class ResultsPageComponent implements OnInit {
   @Input() versionNum?: number | undefined;
   project: Project | undefined;
   projectHistory: ProjectVersion<Project>[] = [];
-  constructor(private projectService: ProjectService<Project>, private jsonService: JsonService) {}
+  sectionInputStats: { [key: string]: number | string } = {};
+  latestVersionNum: number | undefined;
+
+  constructor(
+    private projectService: ProjectService<Project>,
+    private jsonService: JsonService,
+    private statisticsService: StatisticsService
+  ) {}
 
   ngOnInit(): void {
     if (this.projectId !== undefined) {
       this.project = this.projectService.getProjectVersion(this.projectId, this.versionNum || 1);
       this.projectHistory = this.projectService.getProjectHistory(this.projectId);
+      this.latestVersionNum = this.projectHistory.length > 0 ? this.projectHistory[this.projectHistory.length - 1].versionNum : undefined;
+      this.calculateSectionInputStats();
     }
   }
 
@@ -46,6 +56,12 @@ export class ResultsPageComponent implements OnInit {
       if (this.project) {
         this.jsonService.saveProjectToJson(this.project);
       }
+    }
+  }
+
+  private calculateSectionInputStats(): void {
+    if (this.project) {
+      this.sectionInputStats = this.statisticsService.calculateSectionInputStats(this.project);
     }
   }
 }
