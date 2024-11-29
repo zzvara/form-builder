8b7ac3d3079f3380cbfef791b253e8e64bf23419
hxffner <hofi.marcell@gmail.com>
2024-11-29 08:53:58 +0100

    Upgrade Angular dependencies to version 18 and refactor HttpClient import

--- form-builder/src/app/app.module.ts
+++ form-builder/src/app/app.module.ts
@@ -7,7 +7,7 @@ import {en_US, NZ_I18N} from 'ng-zorro-antd/i18n';
 import {registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DashboardModule} from './pages/dashboard/dashboard.module';
 import {EditModule} from './pages/edit/edit.module';
@@ -17,22 +17,15 @@ import {NzCheckboxModule} from 'ng-zorro-antd/checkbox';
 
 registerLocaleData(en);
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    DashboardModule,
-    EditModule,
-    SharedModule,
-    FormCreatorModule,
-    NzCheckboxModule,
-  ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        DashboardModule,
+        EditModule,
+        SharedModule,
+        FormCreatorModule,
+        NzCheckboxModule], providers: [{ provide: NZ_I18N, useValue: en_US }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
