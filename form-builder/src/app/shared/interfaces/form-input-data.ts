9d1b19d82cc8a596ad4269df11ea89d8e8c38bcd
kovacstomi121 <kovacstomi121@gmail.com>
2025-04-11 11:39:40 +0200

    Component page delete placeholders

--- form-builder/src/app/shared/interfaces/form-input-data.ts
+++ form-builder/src/app/shared/interfaces/form-input-data.ts
@@ -3,8 +3,6 @@ import { InputData } from '@interfaces/input-data';
 
 export interface FormInputData<D extends InputData<T> = InputData, T = any> {
   title: string;
-  questionPlaceholder?: string;
-  descriptionPlaceholder?: string;
   type: keyof typeof translateComponentType;
   data: D | null;
 }
