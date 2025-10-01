1dcc61b12958c7d0cfb7daeda5ae5fa24074719c
Oravecz József <orav.joko@gmail.com>
2025-10-01 18:57:50 +0200

    #165 Used DateFormat

--- src/app/shared/components/date-picker/interfaces/default-date-formats.ts
+++ src/app/shared/components/date-picker/interfaces/default-date-formats.ts
@@ -1,3 +1,4 @@
+import { DateFormat } from '@app/shared/constants/date-format.constant';
 import { NzDateMode } from 'ng-zorro-antd/date-picker';
 
 export type DefaultDateFormats = {
@@ -5,11 +6,11 @@ export type DefaultDateFormats = {
 };
 
 export const defaultDateFormats: DefaultDateFormats = {
-  date: 'yyyy-MM-dd',
-  decade: "yyyy'''s'",
-  year: 'yyyy',
-  month: 'yyyy-MM',
-  week: 'yyyy-ww',
-  time: 'HH:mm:ss',
-  quarter: 'yyyy-Q',
+  date: DateFormat.DATE,
+  decade: DateFormat.DECADE,
+  year: DateFormat.YEAR,
+  month: DateFormat.MONTH,
+  week: DateFormat.WEEK,
+  time: DateFormat.TIME,
+  quarter: DateFormat.QUARTER,
 };
--- src/app/shared/constants/date-format.constant.ts
+++ src/app/shared/constants/date-format.constant.ts
@@ -3,4 +3,9 @@ export class DateFormat {
   public static readonly TIME = 'HH:mm:ss';
   public static readonly DATE = 'yyyy.MM.dd';
   public static readonly HOUR_MINUTE = 'HH:mm';
+  public static readonly DECADE = "yyyy'''s'";
+  public static readonly YEAR = 'yyyy';
+  public static readonly MONTH = 'yyyy.MM';
+  public static readonly WEEK = 'yyyy.ww';
+  public static readonly QUARTER = 'yyyy.Q';
 }
