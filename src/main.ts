b083d3b5e0a8a5ff03e1e7d8bc6703ea62060fef
Kristóf Bederna <bederna.kristof@gmail.com>
2026-03-09 11:53:42 +0100

    #101 added a version counter that pops up in the console exactly once on initial visitation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from '@app/app.module';
+import { version } from './version';
+
+console.log('App version:', version);
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
