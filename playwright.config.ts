0f099cca9e683c85565199e1787bdeadc2b93d4e
Kristóf Bederna <bederna.kristof@gmail.com>
2026-03-18 08:52:41 +0100

    #223 seperated build and test workflows, built project gets handed down as an artifact, works locally, should work in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,12 +6,17 @@ export default defineConfig({
   forbidOnly: !!process.env['CI'],
   retries: process.env['CI'] ? 2 : 0,
   workers: process.env['CI'] ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html'], ['github']],
+
+  use: {
+    baseURL: 'http://localhost:4200',
+    video: 'on',
+  },
 
   webServer: {
     command: 'npx serve -s dist/form-builder/browser -l 4200',
-    port: 4200,
-    reuseExistingServer: true,
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env['CI'],
     timeout: 120 * 1000,
   },
 
