0d0011f6efde15c1164cb8b4c5902f6131f25380
matelazar07 <matelazar07@gmail.com>
2026-03-26 22:42:34 +0100

    #230 issue-230-zoneless-and-signals remove unused variables and refactor logic - set signals in header and signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 import { HeaderService } from '@services/header/header.service';
 import { MenuOption } from '@models/menu-option.model';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { NzLayoutComponent } from 'ng-zorro-antd/layout';
 
-/**
- * @todo It seems as if this component doesn't contain logic that's actually useful at this point in time.
- * It should be simplified. Details below.
- */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NzLayoutComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit, OnDestroy {
-  activeOptions: MenuOption[] = []; // @todo Unused variable. A value is given, but never used.
-  optionsSub?: Subscription; // @todo Unused variable.
-  options = MenuOption; // @todo Unused variable.
-
-  title = 'form-builder'; // @todo Unused variable. The related test must be aligned after changing this.
-
-  constructor(private readonly headerService: HeaderService) {}
-
-  ngOnInit(): void {
-    this.headerService
-      .getOptions()
-      .subscribe((options) => (this.activeOptions = options.activeOptions));
-  }
-
-  ngOnDestroy(): void {
-    this.optionsSub?.unsubscribe(); // @todo Unnecessary lifecycle hook.
-  }
+export class AppComponent {
 }
