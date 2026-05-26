b076685a2a79259ecf554e976db571152931593b
matelazar07 <matelazar07@gmail.com>
2026-05-26 20:29:32 +0200

    #230 fix edit button and undo redo functionality, delete zone,js and set zoneless + select-edit.component.ts

--- src/app/shared/components/select/select-edit/select-edit.component.ts
+++ src/app/shared/components/select/select-edit/select-edit.component.ts
@@ -35,6 +35,10 @@ export class SelectEditComponent extends AbstractFieldLikeEditForm<string | stri
     return [];
   }
 
+  get isMultipleChoice(): boolean {
+    return this.getStrictControlValue<boolean>('isMultipleChoice') ?? false;
+  }
+
   get newOptionValue(): string | null {
     return this.newOption.getRawValue();
   }
